8539aff2deacc086afae03da62543e722a723c06
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2023-08-20 11:41:02 -0500

    Delete prints

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from archinstall.Menu import ConfigMenu
 if not os.geteuid():
     print("Bienvenido a PyArchInstall")
     if os.path.exists("/var/lib/pacman/db.lck"):
-        print("El archivo existe")
         os.system("sudo rm /var/lib/pacman/db.lck")
     else:
-        print("El archivo no existe")
         appsInstall = ConfigMenu()
 else:
     print("Vuelve cuando seas root")
