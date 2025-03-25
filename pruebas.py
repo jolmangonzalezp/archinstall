a676bcedbc415471ebfbbcb26ba524ae54d2e50a
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2025-03-24 19:51:04 -0500

    python scrypt

--- pruebas.py
+++ pruebas.py
@@ -1,17 +1,39 @@
 import subprocess
 
 
-def install():
-    cpu_brand = ""
-    result = subprocess.run(
-        ["lscpu"], capture_output=True, text=True, check=True
-    )
-    # Filtrar la línea con "Vendor ID:" y extraer la tercera columna
-    for line in result.stdout.splitlines():
-        if "Vendor ID:" in line:
-            cpu_brand = line.split()[2]
+def format_disk():
+    global efi, root_partition, disk_name
+    # interface = subprocess.run(
+    #     ["lsblk", "-o", "NAME,TRAN"], capture_output=True, text=True
+    # ).stdout.splitlines()
+    interface = ""
+    try:
+        result = subprocess.run(
+            ["lsblk", "-o", "NAME,TRAN"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        for line in result.stdout.splitlines():
+            if line.startswith(disk_name + " "):
+                interface = line.split()[1]
 
-    # Imprimir el nombre del procesador
-    print(f"Nombre del procesador: {cpu_brand}")
+        if "sata" in interface:
+            efi = f"{disk_name}1"
+            root_partition = f"{disk_name}2"
+        elif "nvme" in interface:
+            efi = f"{disk_name}p1"
+            root_partition = f"{disk_name}p2"
+            print(f"Interfaz de {disk_name}: {interface}")
+        else:
+            print(f"No se pudo determinar la interfaz de {disk_name}.")
+            
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar lsblk: {e}")
+        interface = None
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        interface = None
     
-install()
+format_disk()
